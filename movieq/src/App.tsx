115f1fa1cf22b5147792388b17601b7bb245c90e
ylyas2004 <yylkybayewylyas@gmail.com>
2025-03-12 12:54:28 +0300

    Update App.tsx

--- movieq/src/App.tsx
+++ movieq/src/App.tsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Furkan + Sayar </h1>
+      <h1>Ylyas + Yylkybayev </h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
